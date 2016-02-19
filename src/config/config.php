a98a2f765a5c4ddc7012a7df32644ca18ea65db4
Ian <ian@digiserv.net>
2016-02-19 03:48:38 +0000

    Add copyright headers.

--- src/config/config.php
+++ src/config/config.php
@@ -1,11 +1,17 @@
 <?php
 /**
- * LFSWorld Pubstats Package
+ * This file is part of the icawebdesign/LFSW-Pubstats library
  *
- * Configuration file
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  *
- * @author Ian.H <ian.h@icawebdesign.co.uk>
+ * @copyright Copyright (c) 2006-2016. Ian.H <ian.h@icawebdesign.co.uk>
+ * @license http://opensource.org/licenses/MIT MIT
+ * @link https://github.com/icawebdesign/LFSW-Pubstats GitHub
  */
- return [
+
+
+
+return [
      'IDKEY'            => 'LFSWORLD PUBSTATS IDKEY',
  ];
--- tests/src/LfswPubstatsTest.php
+++ tests/src/LfswPubstatsTest.php
@@ -1,4 +1,17 @@
 <?php
+/**
+ * This file is part of the icawebdesign/LFSW-Pubstats library
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @copyright Copyright (c) 2006-2016. Ian.H <ian.h@icawebdesign.co.uk>
+ * @license http://opensource.org/licenses/MIT MIT
+ * @link https://github.com/icawebdesign/LFSW-Pubstats GitHub
+ */
+
+
+
 namespace Src;
 
 use Icawebdesign\LfswPubstats\LfswPubstats;
