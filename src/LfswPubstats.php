1ce97298edf987dd9a12bb7de93633f88f261df9
Ian <ian@digiserv.net>
2016-02-20 02:10:26 +0000

    Add minimum PHP version to composer. Add online host retrieval. This needs to have the track list expanded / completed.
    Contributes to:
      LFSWPSTATS-7

--- src/LfswPubstats.php
+++ src/LfswPubstats.php
@@ -33,6 +33,149 @@ class LfswPubstats
      * @var string
      */
     protected $dotenvFile = '.env';
+    /**
+     * @var string
+     */
+    protected $lfswUrl = '';
+
+    const carCodes = [
+        'XFG'           => 'XF GTi',
+        'XRG'           => 'XR GT',
+        'XRT'           => 'XR GT Turbo',
+        'RB4'           => 'RB4 GT',
+        'FXO'           => 'FXO Turbo',
+        'LX4'           => 'LX4',
+        'LX6'           => 'LX6',
+        'MRT'           => 'MRT5',
+        'UF1'           => 'UF1000',
+        'RAC'           => 'RaceAbout',
+        'FZ5'           => 'FZ50',
+        'FOX'           => 'Formula XR',
+        'XFR'           => 'XF GTR',
+        'UFR'           => 'UF GTR',
+        'FO8'           => 'Formula V8',
+        'FXR'           => 'FXO GTR',
+        'XRR'           => 'XR GTR',
+        'FZR'           => 'FZ GTR',
+    ];
+
+    const carBits = [
+        1               => 'XFG',
+        2               => 'XRG',
+        4               => 'XRT',
+        8               => 'RB4',
+        16              => 'FXO',
+        32              => 'LX4',
+        64              => 'LX6',
+        128             => 'MRT',
+        256             => 'UF1',
+        512             => 'RAC',
+        1024            => 'FZ5',
+        2048            => 'FOX',
+        4096            => 'XFR',
+        8192            => 'UFR',
+        16384           => 'FO8',
+        32768           => 'FXR',
+        65536           => 'XRR',
+        131072          => 'FZR',
+    ];
+
+    const optionBits = [
+        0               => 'WHEELSTEER',
+        1               => 'LEFTHANDDRIVE',
+        2               => 'GEARCHANGECUT',
+        4               => 'GEARCHANGEBLIP',
+        8               => 'AUTOGEAR',
+        16              => NULL,
+        32              => NULL,
+        64              => 'BRAKEHELP',
+        128             => 'THROTTLEHELP',
+        256             => NULL,
+        512             => NULL,
+        1024            => 'MOUSESTEER',
+        2048            => 'KEYBOARDSTEERHOHELP',
+        4096            => 'KEYBOARDSTEERSTABILISED'
+    ];
+
+    const trackCodes = [
+        '000'           => 'Blackwood GP',
+        '001' 			=> 'Blackwood GP reversed',
+        '010' 			=> 'Blackwood RallyX',
+        '011' 			=> 'Blackwood RallyX reversed',
+        '020' 			=> 'Blackwood Car Park',
+        '100' 			=> 'South City Classic',
+        '101' 			=> 'South City Classic reversed',
+        '110' 			=> 'South City Sprint Track 1',
+        '111' 			=> 'South City Sprint Track 1 reversed',
+        '120' 			=> 'South City Sprint Track 2',
+        '121' 			=> 'South City Sprint Track 2 reversed',
+        '130' 			=> 'South City Long',
+        '131' 			=> 'South City Long reversed',
+        '140' 			=> 'South City Town Course',
+        '141' 			=> 'South City Town Course reversed',
+        '200' 			=> 'Fern Bay Club',
+        '201' 			=> 'Fern Bay Club reversed',
+        '210' 			=> 'Fern Bay Green',
+        '211' 			=> 'Fern Bay Green reversed',
+        '220' 			=> 'Fern Bay Gold',
+        '221' 			=> 'Fern Bay Gold reversed',
+        '230' 			=> 'Fern Bay Black',
+        '231' 			=> 'Fern Bay Black reversed',
+        '240' 			=> 'Fern Bay Rallycross',
+        '241' 			=> 'Fern Bay Rallycross reversed',
+        '250' 			=> 'Fern Bay Rally X Green',
+        '251' 			=> 'Fern Bay Rally X Green reversed',
+        '300' 			=> 'Autocross',
+        '310' 			=> 'Autocross Skid pad',
+        '320' 			=> 'Autocross Drag strip',
+        '330' 			=> 'Autocross 8 lane drag strip',
+        '400' 			=> 'Kyoto Ring Oval',
+        '401' 			=> 'Kyoto Ring Oval reversed',
+        '410' 			=> 'Kyoto Ring National',
+        '411' 			=> 'Kyoto Ring National reversed',
+        '420' 			=> 'Kyoto Ring GP Long',
+        '421' 			=> 'Kyoto Ring GP Long reversed',
+        '500' 			=> 'Westhill International',
+        '501' 			=> 'Westhill International reversed',
+        '600' 			=> 'Aston Cadet',
+        '601' 			=> 'Aston Cadet reversed',
+        '610' 			=> 'Aston Club',
+        '611' 			=> 'Aston Club reversed',
+        '620' 			=> 'Aston National',
+        '621' 			=> 'Aston National reversed',
+        '630' 			=> 'Aston Historic',
+        '631' 			=> 'Aston Historic reversed',
+        '640' 			=> 'Aston Grand prix',
+        '641' 			=> 'Aston Grand prix reversed',
+    ];
+
+    const ruleCodes = [
+        'V'             => 'Can Vote',
+        'S'             => 'Can Select',
+        'Q'             => 'Qualifying',
+        'P'             => 'Private',
+        'M'             => 'Modified',
+    ];
+
+    const ruleBits = [
+        1               => 'Can Vote',
+        2               => 'Can Select',
+        4               => 'Qualifying',
+        8               => 'Private',
+        16              => 'Modified',
+        32              => 'Mid-race Join',
+        64              => 'Must Pit',
+        128             => 'Can Reset',
+        256             => 'Force Cockpit View',
+        512             => 'Cruise',
+    ];
+
+    const serverTypes = [
+        'S1 (old)',
+        'S1',
+        'S2',
+        'S3',
+    ];
 
     /**
      * LfswPubstats constructor.
@@ -40,7 +183,7 @@ class LfswPubstats
     public function __construct($dotenvFile = '.env')
     {
         $this->dotenvFile = $dotenvFile;
-        $this->readConfig(include_once __DIR__ . '/config/config.php');
+        $this->readConfig(include __DIR__ . '/config/config.php');
 
         return $this;
     }
@@ -89,12 +232,13 @@ class LfswPubstats
         // Read .env file if it exists
         if (file_exists(__DIR__ . '/../.env')) {
             $dotenv = new Dotenv(__DIR__ . '/..', $this->dotenvFile);
-            $env = $dotenv->load();
+            $env = $dotenv->overload();
 
             try {
                 $dotenv->required('IDKEY')->notEmpty();
             } catch (\Exception $e) {
-                die(trigger_error($e->getMessage(), E_USER_ERROR));
+                trigger_error($e->getMessage(), E_USER_ERROR);
+                die();
             }
 
             foreach ($env as $line) {
@@ -105,7 +249,6 @@ class LfswPubstats
 
                 if (false !== strpos($line, '=')) {
                     list($key, $value) = array_map('trim', explode('=', $line, 2));
-
                     $this->config[$key] = str_replace(['export ', "'", '"'],  '', $value);
                 }
             }
@@ -116,11 +259,173 @@ class LfswPubstats
 
     /**
      * Get config data
+     *
+     * @param mixed null|string $key
+     * @return mixed string|array
+     */
+    public function getConfig($key = null)
+    {
+        if (null !== $key) {
+            return $this->config[$key];
+        }
+
+        return $this->config;
+    }
+
+    /**
+     * Set LFSWorld URL with query string for action and params
+     *
+     * @param array $queryString
+     *
+     * @return $this
+     */
+    protected function setLfswUrl(array $queryString = [])
+    {
+        $this->lfswUrl = $this->getConfig('LFSW_URL') .
+                         "?idk={$this->getConfig('IDKEY')}" .
+                         '&s=1' .
+                         '&' . http_build_query($queryString, null, '&');
+        
+        if (null !== $this->getConfig('API_VERSION')) {
+            $this->lfswUrl .= "&version={$this->getConfig('API_VERSION')}";
+        }
+
+        return $this;
+    }
+
+    /**
+     * Parse colour codes (^1) into CSS class names
      * 
+     * @param string $data
+     *
+     * @return string
+     */
+    protected function parseColourCodes($data)
+    {
+        // Return base data if no colour control char is found
+        if (false === strpos($data, '^')) {
+            return $data;
+        }
+        
+        $colourMap = [
+            '0'             => 'lfs-black',
+            '1'             => 'lfs-red',
+            '2'             => 'lfs-green',
+            '3'             => 'lfs-yellow',
+            '4'             => 'lfs-blue',
+            '5'             => 'lfs-magenta',
+            '6'             => 'lfs-cyan',
+            '7'             => 'lfs-white',
+            '8'             => 'lfs-default',
+        ];
+
+        $data = preg_replace_callback(
+            '`\^([0-8])(.*?)(?=(?:\^[0-8])|\z)`',
+            function($matches) use ($colourMap) {
+                return "<span class=\"{$colourMap[$matches[1]]}\">{$matches[2]}</span>";
+            },
+            $data
+        );
+
+        return $data;
+    }
+
+    /**
+     * Convert track code (000) into full name
+     *
+     * @param string $trackCode
+     *
+     * @return mixed string|null
+     */
+    protected function parseTrackCode($trackCode)
+    {
+        if (array_key_exists($trackCode, self::trackCodes)) {
+            return self::trackCodes[$trackCode];
+        }
+
+        return null;
+    }
+
+    /**
+     * Parse car bits into car codes and fullnames
+     *
+     * @param int $cars
+     *
      * @return array
      */
-    public function getConfig()
+    protected function parseCarBits($cars)
     {
-        return $this->config;
+        $carsList = [];
+
+        foreach (self::carBits as $bit => $car) {
+            if ($cars === ($cars | $bit)) {
+                $carsList[$car]['code'] = $car;
+                $carsList[$car]['full_name'] = self::carCodes[$car];
+            }
+        }
+
+        return $carsList;
+    }
+
+    /**
+     * Parse rule bits into rule names
+     *
+     * @param int $rules
+     *
+     * @return array
+     */
+    protected function parseRuleBits($rules)
+    {
+        $rulesList = [];
+
+        foreach (self::ruleBits as $bit => $rule) {
+            if ($rules === ($rules | $bit)) {
+                $rulesList[] = $rule;
+            }
+        }
+
+        return $rulesList;
+    }
+
+    /**
+     * Remove any ingame colour codes (^1)
+     *
+     * @param string $data
+     *
+     * @return string
+     */
+    protected function stripColourCodes($data)
+    {
+        if (false === strpos($data, '^')) {
+            return $data;
+        }
+
+        return preg_replace('@\^[0-8]@', '', $data);
+    }
+
+    /**
+     * Determine race distance in laps or hours
+     *
+     * @param int $laps
+     *
+     * @return array
+     */
+    protected function getRaceDurations($laps)
+    {
+        $durations = [
+            'laps'          => $laps,
+            'hours'         => 0,
+        ];
+
+        if ($laps > 100) {
+            if ($laps < 191) {
+                $durations['laps'] = ((($laps - 100) * 10) + 100);
+            } else {
+                $durations['laps'] = 0;
+                $durations['hours'] = $laps - 190;
+            }
+        }
+
+        return $durations;
     }
 }
--- src/config/config.php
+++ src/config/config.php
@@ -13,5 +13,7 @@
 
 
 return [
-     'IDKEY'            => 'LFSWORLD PUBSTATS IDKEY',
- ];
+    'IDKEY'             => 'LFSWORLD PUBSTATS IDKEY',
+    'LFSW_URL'          => 'http://www.lfsworld.net/pubstat/get_stat2.php',
+    'API_VERSION'       => null,      // 1.1, 1.2, 1.3, 1.4, 1.5, null for latest version
+];
