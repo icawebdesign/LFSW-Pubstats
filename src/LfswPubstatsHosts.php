77456290257d80e4498f8b9a806968e5b506c431
Ian <ian@digiserv.net>
2016-02-29 00:25:07 +0000

    Refactor methods to use getLfswData() method instead of multiple fopen() / fread() calls.

--- src/LfswPubstatsHosts.php
+++ src/LfswPubstatsHosts.php
@@ -27,51 +27,39 @@ class LfswPubstatsHosts extends LfswPubstats
         return parent::__construct();
     }
 
+    /**
+     * Get collection of online hosts
+     *
+     * @return array
+     */
     public function getHostsOnline()
     {
         $this->setLfswUrl(['action' => 'hosts']);
-        $hostData = '';
+        $hostData = $this->getLfswData();
 
-        try {
-            $fp = fopen($this->lfswUrl, 'rb');
-        } catch (\Exception $e) {
-            trigger_error($e->getMessage(), E_USER_ERROR);
-            die();
+        $hostList = json_decode($hostData);
+
+        foreach ($hostList as &$host) {
+            $host->hostnameColour = $this->parseColourCodes($host->hostname);
+            $host->hostnamePlain = $this->stripColourCodes($host->hostname);
+
+            $host->tcrm = unpack('c1track/c1config/c1track_rev/c1max_players', $host->tcrm);
+            $trackNumber = "{$host->tcrm['track']}{$host->tcrm['config']}{$host->tcrm['track_rev']}";
+            $host->track = $this->parseTrackNumber($trackNumber);
+
+            $host->tmlt = unpack('c1server_type/c1version/c1patch/c1test_version', $host->tmlt);
+            $host->tmlt['version'] /= 10;
+            $host->tmlt['patch'] = chr($host->tmlt['patch']);
+            $host->carsList = $this->parseCarBits($host->cars);
+            $host->rulesList = $this->parseRuleBits($host->rules);
+
+            $host->raceDuration = $this->getRaceDurations($host->laps);
+            $host->practice = (
+                (0 === $host->raceDuration['laps']) &&
+                (0 === $host->raceDuration['hours']
+            ) ? true : false);
         }
 
-        if (is_resource($fp)) {
-            while (!feof($fp)) {
-                $hostData .= fread($fp, 1024);
-            }
-
-            $hostList = json_decode($hostData);
-
-            fclose($fp);
-
-            foreach ($hostList as &$host) {
-                $host->hostnameColour = $this->parseColourCodes($host->hostname);
-                $host->hostnamePlain = $this->stripColourCodes($host->hostname);
-
-                $host->tcrm = unpack('c1track/c1config/c1track_rev/c1max_players', $host->tcrm);
-                $trackNumber = "{$host->tcrm['track']}{$host->tcrm['config']}{$host->tcrm['track_rev']}";
-                $host->track = $this->parseTrackNumber($trackNumber);
-
-                $host->tmlt = unpack('c1server_type/c1version/c1patch/c1test_version', $host->tmlt);
-                $host->tmlt['version'] /= 10;
-                $host->tmlt['patch'] = chr($host->tmlt['patch']);
-                $host->carsList = $this->parseCarBits($host->cars);
-                $host->rulesList = $this->parseRuleBits($host->rules);
-
-                $host->raceDuration = $this->getRaceDurations($host->laps);
-                $host->practice = (
-                    (0 === $host->raceDuration['laps']) &&
-                    (0 === $host->raceDuration['hours']
-                ) ? true : false);
-            }
-
-            return $hostList;
-        }
-
-        return null;
+        return $hostList;
     }
 }
